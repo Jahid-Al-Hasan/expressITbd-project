bca48b94286526475936386331e962070be9b2a2
Jahid <hzahid905@gmail.com>
2025-02-20 13:41:55 +0600

    btn

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,9 @@
-function Button() {
+function Button({ btnType }) {
   return (
-    <button className="btn text-sm font-bold px-5 py-2 rounded-md border-0 bg-[#C1C2F9] text-white">
+    <button
+      type={btnType}
+      className="btn text-sm font-bold px-5 py-2 rounded-md border-0 bg-[#C1C2F9] text-white"
+    >
       Create store
     </button>
   );
